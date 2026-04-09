3d50142c8fc74f671df382ac4bcfafe79f4d18f0
satyamray940-hue <satyamray940@gmail.com>
2026-04-09 19:28:37 +0530

    fix csrf

--- youtubeveg/settings.py
+++ youtubeveg/settings.py
@@ -129,3 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = ['https://web-production-dc5f8.up.railway.app']
