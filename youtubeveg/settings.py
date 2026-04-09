0aacd5b4054c1f49aaa3c5210b84edde83c84eac
satyamray940-hue <satyamray940@gmail.com>
2026-04-09 19:33:20 +0530

    fix static files

--- youtubeveg/settings.py
+++ youtubeveg/settings.py
@@ -129,4 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CSRF_TRUSTED_ORIGINS = ['https://web-production-dc5f8.up.railway.app']
